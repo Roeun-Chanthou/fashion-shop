8a3bdbaa08d7c90f93af1baa7da9cde85ad89bea
Roeun-Chanthou <roeunchanthou1401@gmail.com>
2025-01-26 23:30:35 +0700

    update product

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:fashion_shop/data/datasources/clothes_ds.dart';
+import 'package:fashion_shop/data/datasources/product_ds.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,19 +10,288 @@ class HomeScreen extends StatelessWidget {
 
   HomeScreen({super.key});
 
-  late double width;
-  late double height;
+  late final double width;
+  late final double height;
 
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       body: CustomScrollView(
         slivers: [
           _buildSliverAppBar(),
-          
+          _buildListNew(),
+          _buildListProduct(),
+          _buildItemProduct(),
         ],
+      ),
+    );
+  }
+
+  Widget _buildListProduct() {
+    return SliverPadding(
+      padding: EdgeInsets.only(
+        bottom: 16,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate(
+          [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+                left: 16,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sale",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "You've never seen it before!",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "View all",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemProduct() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            var item = products[index];
+            var data = Product.fromJson(item);
+
+            return Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        data.images.last,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    data.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    '\$${data.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.green,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 16,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        data.rating.toStringAsFixed(1),
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+          childCount: products.length,
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListNew() {
+    return SliverPadding(
+      padding: EdgeInsets.only(
+        top: 20,
+        bottom: 16,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate(
+          [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+                left: 16,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "New",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "You've never seen it before!",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "View all",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            _buildItemList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemList() {
+    return SizedBox(
+      height: height * 0.4,
+      child: ListView.separated(
+        separatorBuilder: (context, index) {
+          return SizedBox(
+            width: 20,
+          );
+        },
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 16,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: clothes.length,
+        itemBuilder: (context, index) {
+          var item = clothes[index];
+          var data = ClothesDs.formJson(item);
+          return SizedBox(
+            width: width * 0.5,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 16,
+              ),
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Image.network(
+                        data.image,
+                        // height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    data.titel,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    data.description,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Price: \$${data.price}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Rating: ${data.rating['rate']} (${data.rating['count']} reviews)',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
